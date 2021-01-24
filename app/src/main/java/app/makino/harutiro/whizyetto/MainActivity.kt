a06c0c75751048b970d53be9e9afb23cec3838cb
harutiro <harutiro2027@outlook.jp>
2021-01-24 23:48:49 +0900

    add:　アラートダイアグラムの勉強

--- app/src/main/java/app/makino/harutiro/whizyetto/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/whizyetto/MainActivity.kt
@@ -1,11 +1,40 @@
 package app.makino.harutiro.whizyetto
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            AlertDialog.Builder(this) // FragmentではActivityを取得して生成
+                    .setTitle("タイトル")
+                    .setMessage("メッセージ")
+                    .setPositiveButton("OK", { dialog, which ->
+                        // TODO:Yesが押された時の挙動
+                    })
+                    .show()
+        }
+
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            AlertDialog.Builder(this) // FragmentではActivityを取得して生成
+                    .setTitle("タイトル")
+                    .setMessage("メッセージ")
+                    .setPositiveButton("OK", { dialog, which ->
+                        // TODO:Yesが押された時の挙動
+                    })
+                    .setNegativeButton("No", { dialog, which ->
+                        // TODO:Noが押された時の挙動
+                    })
+                    .show()
+        }
     }
+
+
+
 }
